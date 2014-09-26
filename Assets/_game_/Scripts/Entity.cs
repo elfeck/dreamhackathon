37285b259c02f2a19845c221bb1a1644bf15abe5
Julian Mautner <julian@mautner.it>
2014-09-26 23:13:41 +0200

    added grid

--- Assets/_game_/Scripts/Entity.cs
+++ Assets/_game_/Scripts/Entity.cs
@@ -1,13 +1,48 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Entity : MonoBehaviour
 {
+	static private List<HashSet<Entity>> _grid;
+
 	public float bias = 0f;
+	private int _currGridIndex = -1;
 
 	void Awake()
 	{
 		bias = Random.Range(-1f, 1f);
+	}
+
+	IEnumerator Start()
+	{
+		if(_grid == null)
+		{
+			_grid = new List<HashSet<Entity>>(2048);
+			var res = GameSession.inst.getGridResolution();
+			res *= res;
+			for(int i = 0; i < res; ++i)
+				_grid.Add(new HashSet<Entity>());
+		}
+
+		//========================================//
+
+		_currGridIndex = getGridIndex();
+		_grid[_currGridIndex].Add(this);
+
+		while(true)
+		{
+			//update grid position
+			var index = getGridIndex();
+			if(_currGridIndex != index)
+			{
+				_grid[_currGridIndex].Remove(this);
+				_currGridIndex = index;
+				_grid[_currGridIndex].Add(this);
+			}
+
+			yield return new WaitForSeconds(Random.Range(0.5f, 1f) * 0.25f);
+		}
 	}
 
 	public void applyBias(float change)
@@ -19,5 +54,10 @@ public class Entity : MonoBehaviour
 	void Update()
 	{
 		renderer.material.color = Color.Lerp(Color.red, Color.blue, Mathf.Clamp01((bias + 1f) / 2f));
+	}
+
+	int getGridIndex()
+	{
+		return GameSession.inst.getGridIndex(transform.position);
 	}
 }
--- Assets/_game_/Scripts/GameSession.cs
+++ Assets/_game_/Scripts/GameSession.cs
@@ -7,7 +7,13 @@ public class GameSession : SASSingleton<GameSession>
 	public GameObject entity;
 	public int totalCount = 100;
 
+	public float gridCellSize = 3f;
+	public Transform ground;
+	
+	private int gridResolution;
 	private List<Entity> _entities = new List<Entity>();
+
+	public int getGridResolution() {return gridResolution;}
 
 	public override void Awake()
 	{
@@ -21,7 +27,19 @@ public class GameSession : SASSingleton<GameSession>
 			var e = go.GetComponent<Entity>();
 			_entities.Add(e);
 		}
+
+		gridResolution = Mathf.CeilToInt(Mathf.Max(ground.localScale.x, ground.localScale.z) / gridCellSize);
+		print(gridResolution);
 	}
 
 	public List<Entity> getEntities() { return _entities; }
+
+	public int getGridIndex(Vector3 pos)
+	{
+		pos += ground.localScale * 0.5f;
+		int x = Mathf.CeilToInt(pos.x / gridCellSize);
+		int y = Mathf.CeilToInt(pos.z / gridCellSize);
+
+		return gridResolution * y + x;
+	}
 }
