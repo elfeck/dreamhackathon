ccc03f5d9a4d93c9c0c1fc1b1d6709a39c35f32d
Stefan <stf.andersson@gmail.com>
2014-09-26 23:03:29 +0200

    Changed Entity.cs, added Movement.cs

--- Assets/_game_/Scripts/Entity.cs
+++ Assets/_game_/Scripts/Entity.cs
@@ -3,7 +3,21 @@ using System.Collections;
 
 public class Entity : MonoBehaviour
 {
-	public float bias = 0f;
+    public float reach;
+    [HideInInspector] public float bias = 0f;
+
+    [HideInInspector] public Movement movement;
+
+    void Awake()
+    { 
+        movement = GetComponent<Movement>();
+
+        //"Random spawn"
+        Vector3 _tmp = new Vector3();
+        _tmp.x = Random.Range((GameObject.Find("obj_Ground").transform.localScale.x / 2) * -1, (GameObject.Find("obj_Ground").transform.localScale.x / 2));
+        _tmp.z = Random.Range((GameObject.Find("obj_Ground").transform.localScale.z / 2) * -1, (GameObject.Find("obj_Ground").transform.localScale.z / 2));
+        transform.position = _tmp;
+    }
 
 	public void applyBias(float change)
 	{
@@ -15,4 +29,17 @@ public class Entity : MonoBehaviour
 	{
 		renderer.material.color = Color.Lerp(Color.red, Color.blue, Mathf.Clamp01((bias + 10f) / 20f));
 	}
+
+    public void InteractWithTarget()
+    {
+        if (Random.Range(-10, 10) > 0)
+        {
+            //Do good
+        }
+
+        else 
+        { 
+            //Do evil
+        }
+    }
 }
