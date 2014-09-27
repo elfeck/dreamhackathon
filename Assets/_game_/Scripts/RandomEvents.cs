b7fa8247f633b94d555ef96046174ed71074fbaa
Stefan <stf.andersson@gmail.com>
2014-09-27 10:00:46 +0200

    Added Random Events (barely WiP)

---
+++ Assets/_game_/Scripts/RandomEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomEvents : MonoBehaviour {
+
+    public float eventDuration;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator OneSideSpeedUp()
+    { 
+        timer = 0;
+
+        while(timer < )
+        {
+            timer += Time.deltaTime;
+            
+        }
+    }
+}
