009cd9a0d45a11632e9cfa7b023aafd019dd7225
Stefan <stf.andersson@gmail.com>
2014-09-27 18:19:37 +0200

    Finished main menu

--- Assets/_game_/Scripts/Menu.cs
+++ Assets/_game_/Scripts/Menu.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class Menu : MonoBehaviour
 {
 	public Rect window = new Rect(0f, 0f, 200f, 200f);
+    public Texture2D background;
 
 	void OnGUI()
 	{
 		Screen.showCursor = true;
 
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background, ScaleMode.ScaleAndCrop);
+
 		window.x = (Screen.width - window.width) * 0.5f;
-		window.y = (Screen.height - window.height) * 0.5f;
+		window.y = (Screen.height - window.height) * 0.73f;
 
 		GUI.Window(0, window, windowFunc, "");
 	}
@@ -20,7 +23,7 @@ public class Menu : MonoBehaviour
 		if(GUILayout.Button("Tutorial"))
 			Application.LoadLevel("Tutorial");
 
-		GUILayout.Space(15f);
+		GUILayout.Space(10f);
 
 		if(GUILayout.Button("Easy"))
 			Application.LoadLevel("Level01");
@@ -29,7 +32,7 @@ public class Menu : MonoBehaviour
 		if(GUILayout.Button("Hard"))
 			Application.LoadLevel("Level03");
 
-		GUILayout.Space(20f);
+		GUILayout.Space(15f);
 
 		if(GUILayout.Button("Exit"))
 			Application.Quit();
